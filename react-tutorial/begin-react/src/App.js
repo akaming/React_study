e46e83c7d2062c3462112eb91c0650f301bdc9b5
akaming <audals2478@gmail.com>
2020-07-10 23:18:30 +0900

    props 추가

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
+
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
+ 
   return (
-    <>
-      {/*주석처리*/}
-      <Hello 
-        // 이런식으로 주석처리 가능
-      />
-      <div 
-        // 이렇게 주석처리도 가능
-      style={style }>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Hello name="react"/>
   );
 }
 
