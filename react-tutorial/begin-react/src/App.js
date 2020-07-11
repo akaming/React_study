94b2ff79b3bbd22e2292ecd625e7d35550f1da74
akaming <audals2478@gmail.com>
2020-07-11 22:50:55 +0900

    props default 추가

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -5,7 +5,10 @@ import Hello from './Hello';
 function App() {
  
   return (
-    <Hello name="react"/>
+    <>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </>
   );
 }
 
